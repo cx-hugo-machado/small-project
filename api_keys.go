6d0fe75107c806c74f8f046ddc20845e2da8dfea
Hugo Duarte Moreira Machado <hugo.machado@checkmarx.com>
2026-01-27 10:48:12 +0000

    Add MY_TOKEN constant for API authentication
    Added a new constant for the API token.

--- api_keys.go
+++ api_keys.go
@@ -1,3 +1,4 @@
 package api_keys
 
 const MY_API_KEY = "sk-ant-api03-1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u2v3w4x5y6z7a8b9c0d1e2f3g4h5i6j7k8l9m0n1o2p3q4r5s6t7u8v9w0x1y2z3a4b5c6d7e8f9g0h1i2j3k"
+const MY_TOKEN = "ghp_1a2B3c4D5e6F7g8H9i0J1k2L3m4N5o6P7q8R"
