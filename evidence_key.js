fb67cb067926e01e17bbd2ca6df515fd8b95d0bb
Hugo Duarte Moreira Machado <hugo.machado@checkmarx.com>
2026-09-21 16:05:54 +0100

    Update evidence_key.js

--- evidence_key.js
+++ evidence_key.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 
-const model = "gpt-4"; // This is an inline comment
+const model = "gpt-4"; // This is an inline comment - updated
 const client = new OpenAI();
 
 function summarize(text) {
